62472becf591f0e727370a3da2b679af9bf11211
William Blacknall <williamblacknall@gmail.com>
2019-09-20 14:19:23 -0400

    Added calculation functionality with eval function

--- js/script.js
+++ js/script.js
@@ -9,27 +9,30 @@ const input = document.getElementById("input"), // input/output button
 let resultDisplayed = false; // flag to keep an eye on what output is displayed
 
 // numbers is a NodeList object, we need to make it into an array first, then we can map through it...
+
+
+
+
+// adding click handlers to the calculation buttons
 numbers.forEach(function(number){
     number.addEventListener("click", function(e) {
-        if (clear.addEventListener("click", function(e) {
-            input.innerHTML = ""
-        })){
-            input.innerHTML = ""
-        }
-        else{
             input.innerHTML += this.innerHTML;
         }
-    });
-});
+    );
+})
 
-operators.forEach(function(operator){
+;operators.forEach(function(operator){
     operator.addEventListener("click", function(e) {
         input.innerHTML += this.innerHTML;
     });
 });
 
-// adding click handlers to the calculation buttons
-
 // on click of 'equal' button, perform the mathematical operation
+result.addEventListener("click", function(e){
+    input.innerHTML = eval(input.innerHTML)
+})
 
 // clear the input on press of clear
+clear.addEventListener("click", function(e) {
+    input.innerHTML = "";
+})
